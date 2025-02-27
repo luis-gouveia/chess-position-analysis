89c2b35956e8a8a2f3e2323ee5d17dd42542371c
luis-gouveia <luis.gouveia2801@gmail.com>
2025-02-27 16:23:18 +0000

    chore: remove unnecessary comment

--- src/scripts/openingsScripts.ts
+++ src/scripts/openingsScripts.ts
@@ -3,7 +3,4 @@ import openings from '../resources/openings.json'
 export function getOpening(fen: string): string | undefined {
   const op = openings as Record<string, { name: string }>
   return op[fen]?.name
-
-  //if (!(fen in Object.keys(openings))) return
-  //return openings[fen].name
 }
